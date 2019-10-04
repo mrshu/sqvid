31466e8326b02f65b255a78198b8f9d681177a8d
mr.Shu <mr@shu.io>
2019-10-04 15:04:15 +0200

    fix: Make `unique` validator select `column`
    * Instead of using `table.select` make a select directly on the DB and
      use the column, which has previously been correctly extracted.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- sqvid/validators.py
+++ sqvid/validators.py
@@ -21,7 +21,7 @@ def unique(table, column, args=None):
     """
     Check whether values in a column are unique.
     """
-    return table.select(column) \
+    return db.select([column]) \
         .group_by(column) \
         .having(db.func.count(column) > 1)
 
